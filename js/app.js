ce2715c0257338779e41a6a08d5a617f12d31e71
dogc7717-boop <dogc7717@gmail.com>
2026-02-10 00:54:50 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,3 +9,18 @@ document.getElementById("joinBtn").onclick = () => {
 document.getElementById("moveBtn").onclick = () => {
   log.textContent += "Dice rolled 🎲\n";
 };
+const status = document.getElementById("status");
+const log = document.getElementById("log");
+
+/* 👇 هنا تحط الرابط والمفتاح */
+const SUPABASE_URL = "https://dxqpttiffkdrtbwzmvcd.supabase.co";
+const SUPABASE_KEY = "sb_publishable_CWdAdibfoDfxUpALqTyOHQ_Jltfbggt";
+/* 👆 لحد هنا بس */
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+status.textContent = "Supabase متصل ✅";
+log.textContent = "Connection OK\n";

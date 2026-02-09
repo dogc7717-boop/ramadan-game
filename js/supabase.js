c3d113831ac2100aa97dc5c3e4ae278605a760eb
dogc7717-boop <dogc7717@gmail.com>
2026-02-09 23:41:03 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-const SUPABASE_KEY = "YOUR_ANON_PUBLIC_KEY";
+const SUPABASE_URL = "https://ycvmnhkcbdgylybdgyue.supabase.co";
+const SUPABASE_KEY = "sb_publishable__-zX3zqL_4xaQhYY4g8_Rw_1hXcWrSM";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
